19db5b0d81b15323f80521122c952c03f62c9476
Sleeyax <yourd3veloper@gmail.com>
2022-05-08 14:28:11 +0200

    work around gcloud dev-dependency issues
    Actually, 'husky' is also a dev dependnecy but I can't seem to work around that problem so easily (redirecting stderr with 2>/dev/null didn't work unfortunately)

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { initDb } from './database';
 import serviceAccountDev from './database/creds/nftc-dev-firebase-creds.json';
 import serviceAccountProd from './database/creds/nftc-dev-firebase-creds.json'; // TODO: change to prod service account
 
-import { config as loadEnv } from 'dotenv';
+// Try to load local environment variables from .env.
+// If the dotenv dependency is not installed (i.e in prod), then no environment variables will be loaded.
+try {
+  const { config: loadEnv } = require('dotenv');
+  loadEnv();
+} catch (err) {}
+
 import { startServices } from './services';
 import { writer as write } from './writer';
 import { NODE_ENV } from './constants';
-
-// load environment vars
-loadEnv();
 
 // init db connection
 const db = initDb(NODE_ENV === 'dev' ? serviceAccountDev : serviceAccountProd);
