c88fd22f2ea10cec73d68c704774d591de5dada2
Sleeyax <yourd3veloper@gmail.com>
2022-05-08 12:54:09 +0200

    load creds based on constant

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,5 @@ export const DISCORD_APP_ID = process.env.DISCORD_APP_ID || '956292440778743838'
 export const DISCORD_VERIFICATION_URL = process.env.DISCORD_VERIFICATION_URL || 'https://dev.nftcompany.com/';
 export const DISCORD_ADMIN_SERVER_ID = process.env.DISCORD_ADMIN_SERVER_ID || '972614311220494396';
 export const DISCORD_ADMIN_SERVER_MONITOR_CHANNEL = process.env.DISCORD_ADMIN_SERVER_MONITOR_CHANNEL || '972614995915460648';
+
+export const NODE_ENV = process.env.NODE_ENV || 'dev';
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { initDb } from './database';
-// todo: change for prod
-import serviceAccount from './database/creds/nftc-dev-firebase-creds.json';
+import serviceAccountDev from './database/creds/nftc-dev-firebase-creds.json';
+import serviceAccountProd from './database/creds/nftc-dev-firebase-creds.json'; // TODO: change to prod service account
 
 import { config as loadEnv } from 'dotenv';
 import { startServices } from './services';
 import { writer as write } from './writer';
+import { NODE_ENV } from './constants';
 
 // load environment vars
 loadEnv();
 
 // init db connection
-const db = initDb(serviceAccount);
+const db = initDb(NODE_ENV === 'dev' ? serviceAccountDev : serviceAccountProd);
 
 main();
 
