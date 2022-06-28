722eddd80af5daf79d2ed41913db31ebf4d188e8
ngduc <nducus@yahoo.com>
2022-06-28 15:12:52 -0700

    constants: add COLLECT_STATS_ENDPOINT

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,14 @@
 export const TWITTER_BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN || 'CHANGEME';
 
+// http://localhost:9090
+export const MAIN_SERVER_URL = process.env.MAIN_SERVER_URL || 'https://dev.nftcompany.com';
+
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || 'OTU2MjkyNDQwNzc4NzQzODM4.YjuGvA.-XvBeRtmXpVKXAG3W4Q0WKbNRtM';
 export const DISCORD_APP_ID = process.env.DISCORD_APP_ID || '956292440778743838';
-export const DISCORD_VERIFICATION_URL = process.env.DISCORD_VERIFICATION_URL || 'https://dev.nftcompany.com/';
+export const DISCORD_VERIFICATION_URL = process.env.DISCORD_VERIFICATION_URL || `${MAIN_SERVER_URL}/`;
 export const DISCORD_ADMIN_SERVER_ID = process.env.DISCORD_ADMIN_SERVER_ID || '972614311220494396';
 export const DISCORD_ADMIN_SERVER_MONITOR_CHANNEL = process.env.DISCORD_ADMIN_SERVER_MONITOR_CHANNEL || '972614995915460648';
+
+export const COLLECT_STATS_ENDPOINT = `${MAIN_SERVER_URL}/collections/collect-stats?list=`;
 
 export const NODE_ENV = process.env.NODE_ENV || 'dev';
