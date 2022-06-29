323555d8453bb209c827e220b04e47b99411075c
ngduc <nducus@yahoo.com>
2022-06-29 09:20:14 -0700

    minor

--- src/services/index.ts
+++ src/services/index.ts
@@ -56,7 +56,7 @@ export async function startServices(writer: (event: SocialFeedEvent) => Promise<
     // twitter,
     discord,
     // coinmarketcap,
-    // updateSocialStatsTrigger,
+    updateSocialStatsTrigger,
     trendingStatsTrigger
   ];
 
