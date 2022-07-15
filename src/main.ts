1436565149b4557a8b7bf0797d065ab28c756d4a
Sleeyax <yourd3veloper@gmail.com>
2022-07-15 18:49:08 +0200

    Add debug option

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,13 @@ try {
 import { startServices } from './services';
 import { writer as write } from './writer';
 import { NODE_ENV } from './constants';
+import { TwitterApiV2Settings } from 'twitter-api-v2';
+
+// optionally enable twitter API debugging
+TwitterApiV2Settings.debug = !!process.env.DEBUG || false;
+TwitterApiV2Settings.logger = {
+  log: console.info
+};
 
 // init db connection
 const db = initDb(NODE_ENV === 'dev' ? serviceAccountDev : serviceAccountProd);
