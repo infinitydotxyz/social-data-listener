baa4ae335c6f3ff70ea2656ea79e0f49dab70af2
nneverlander <findadit@gmail.com>
2022-08-11 22:17:24 +0200

    discord constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
-export const TWITTER_BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN || 'CHANGEME';
+export const TWITTER_BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN || '';
 
 export const MAIN_API_URL = 'http://localhost:9090';
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || 'OTU2MjkyNDQwNzc4NzQzODM4.YjuGvA.-XvBeRtmXpVKXAG3W4Q0WKbNRtM';
-export const DISCORD_APP_ID = process.env.DISCORD_APP_ID || '956292440778743838';
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || '';
+export const DISCORD_APP_ID = '1007378981915471962';
 export const DISCORD_VERIFICATION_URL = process.env.DISCORD_VERIFICATION_URL || `${MAIN_API_URL}/`;
-export const DISCORD_ADMIN_SERVER_ID = process.env.DISCORD_ADMIN_SERVER_ID || '972614311220494396';
-export const DISCORD_ADMIN_SERVER_MONITOR_CHANNEL = process.env.DISCORD_ADMIN_SERVER_MONITOR_CHANNEL || 'announcements';
+export const DISCORD_ADMIN_SERVER_ID = '972614311220494396';
+export const DISCORD_ADMIN_SERVER_MONITOR_CHANNEL = 'announcements';
 
 export const NODE_ENV = process.env.NODE_ENV || 'dev';
